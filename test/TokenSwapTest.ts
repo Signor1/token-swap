109d683ef506dccac871b020969a266e17c5f330
signor1 <emmanuelomemgboji@gmail.com>
2024-02-21 16:06:46 +0100

    feat: added another test

--- test/TokenSwapTest.ts
+++ test/TokenSwapTest.ts
@@ -1,8 +1,6 @@
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { token } from "../typechain-types/@openzeppelin/contracts";
 
 describe("Token Swap Testing", function () {
   async function deployContracts() {
